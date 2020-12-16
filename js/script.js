b289b78ee487ace4951e75f90fe0da60f8c3ab2a
kimjuneseo <june3929213512@gmail.com>
2020-12-16 22:22:27 +0900

    kimjuneseo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-//write...
+let search = document.querySelector('.search') //버튼 태그를 가져와서 변수로 저장
+let button = search.querySelector('button')
+button.addEventListener('click', nameSearch)
+
+function nameSearch() {
+    let value = search.querySelector('input').value
+    console.log(value)
+}
