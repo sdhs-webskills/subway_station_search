b627441ba6dd501f9377d6e630615c1e6bec9bab
wnsdnn <a24369029@gmail.com>
2020-12-18 23:58:32 +0900

    wnsddn

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 //write...
+const search_key = document.getElementsByName('search_key')[0]; //name이 search_key이라는 객체를 가져옴
+search_key.classList.add('input');
+let input_value = new String(); // input_value이라는 변수를 문자열로 복제?
 
 window.onload = function(){
     Search();
     AutoComplete();
+    //recommend();
     //const target = 'Is this all there is';
     //const regExp = /.../ig;
     //console.log(target.match(regExp));
 }
+
+
+// function recommend() {
+    //let value = search.querySelector('input').value //value에 input
+    //value = value.replace(" ", ""); //value 튀어쓰기를 공백으러 바꾸기
+    //if (value != "") { //input받은 값이 공백이 아니면 if문 실행
+    //    let rrr = staitionData.filter((rc) => { //staitionData 배열의 값들을 순차적으로 함수에 인자로 전달하고 실행하는것
+    //        // console.log(rc.station_nm.indexOf(value))
+    //        if (rc.station_nm.indexOf(value) != -1) { //rc에 필터된 station_nm의 input에 친 값이 없으면 -1이 나오는데 -1아 아니면 if 실헹
+    //            let result = rc.station_nm //
+    //            return result
+    //        }
+    //    })
+    //    console.log(rrr)
+    //}
+// }
 
 // 지하철 역명 배열
 const station = [];
@@ -29,39 +49,59 @@ for (let i=0; i < stationList.data.length; i++) {
 
 
 
-const search_key = document.getElementsByName('search_key')[0]; //name이 search_key이라는 객체를 가져옴
-search_key.classList.add('input');
-let input_value = new String(); // input_value이라는 변수를 문자열로 복제?
 
 
 function AutoComplete(){// 자동완성 해주는 함수
-    //search_key.addEventListener('input', function () {
-    //    for(let i=0; i < station.length; i++){
-    //       let p = [i];
-    //       p[i] = document.createElement('p');
-    //       p[i].classList.add('p');
-    //       document.body.appendChild(p[i]);
-    //       p[i].innerText = station[i];
-    //    }
-    //});
+    search_key.addEventListener('input', function () {
+        input_value = this.value;
+        if (input_value != "") { 
+            let stat = stationList.data.filter((data) => { 
+                // console.log(data.station_nm.indexOf(this.value))
+                if (data.station_nm.indexOf(input_value) != -1) { 
+                    // for(let i=0; i<data.s)
+                    let result = data.station_nm
+                    return result
+                }
+            })
+            const result = stat.map((obj) => Object.values(obj)[0]);
+            let autocomplete = document.querySelector('.autocomplete ul');
+            // let li = document.createElement('li');
+            // li.classList.add('li');
+            // autocomplete.appendChild(li)
+            // li.innerText = result
+            // console.log(result);
+            
+            for(let i=0; i<result.length;i++){
+                while (autocomplete.firstChild) autocomplete.removeChild(autocomplete.firstChild)
+                let li =[i];
+                li[i] = document.createElement('li');
+                li[i].classList.add('li');
+                autocomplete.appendChild(li[i])
+                console.log(result[i]);
+                li[i].innerText = result;
+            }
+
+            
+            // console.log(result);
+            // console.log(stat[Object.keys(stat)[0]]);
+            // let a = JSON.stringify(stat)
+            // let b = a.split('-')
+            
+            // for(let i=0; i < station.length; i++){
+            //    let list_li = [i];
+            //    list_li[i] = document.createElement('li');
+            //    list_li[i].classList.add('list_li');
+            //    list_ul.appendChild(list_li[i]);
+            //    list_li[i].innerText = station[i]
+            // }
+            // console.log(Object.entries(stat))
+            // console.log(stat)
+            // console.log(Object.keys(stat))
+            // console.log(a)
+            // li.innerText = JSON.stringify(result)
+        }
+    });
     
-
-
-
-
-
-    //let p = document.createElement('p')
-    //p.classList.add('p');
-    //document.body.appendChild(p);
-    //console.log(str);
-    //search_key.addEventListener('input', function(){
-    //    let station_num = station.indexOf(input_value)
-    //    station.filter(function(value){
-    //        if(value == input_value){   
-    //            p.innerText = station[station_num] +" / "+stationList.data[station_num].line_num+"호선"
-    //        }
-    //    })
-    //});
 };
 
 
