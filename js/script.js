f04a07da8c3af042e1e3d68ee637a437dfec43bd
kimjuneseo <june3929213512@gmail.com>
2020-12-18 23:56:49 +0900

    kimjuneseo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
 const staitionData = stationList.data
+
 let search = document.querySelector('.search') //search 태그를 가져와서 변수로 저장 
 let button = search.querySelector('button') //button 태그를 가져와서 변수로 저장
 let input = document.querySelector('input') //input태그를 가져와서 변수 선언
 input.addEventListener('keyup', recommend) //input에 keyup 되었을때 recommend함수 실행
 button.addEventListener('click', nameSearch) //button에 클릭할때 nameSearch를 실행
 
+
+//자동완성
 function recommend() {
 
     let value = search.querySelector('input').value //value에 input
     value = value.replace(" ", ""); //value 튀어쓰기를 공백으러 바꾸기
     if (value != "") { //input받은 값이 공백이 아니면 if문 실행
-        let rrr = staitionData.filter((rc) => { //staitionData 배열의 값들을 순차적으로 함수에 인자로 전달하고 실행하는것
+        let autoSearch = staitionData.filter((rc) => { //staitionData 배열의 값들을 순차적으로 함수에 인자로 전달하고 실행하는것
             // console.log(rc.station_nm.indexOf(value))
             if (rc.station_nm.indexOf(value) != -1) { //rc에 필터된 station_nm의 input에 친 값이 없으면 -1이 나오는데 -1아 아니면 if 실헹
                 let result = rc.station_nm //
                 return result
             }
         })
-        console.log(rrr)
+        let autocomplete = document.querySelector('.autocomplete ul')
+        while (autocomplete.firstChild) autocomplete.removeChild(autocomplete.firstChild)
+        let searchResult = autoSearch.map((obj) => Object.values(obj)[0]);
+        // console.log(searchResult)
+        let aa = document.createElement('li')
+        aa.innerText = searchResult
+        autocomplete.appendChild(aa)
+        autocomplete.appendChild(aa)
     }
-
+    // 일단 가져와 태그를
+    // auto밑 ul에 li생성
+    // 그 리스트에 autoSearch을 넣어조
 }
 
+
+
+
+//검색
 function nameSearch() {
     let value = search.querySelector('input').value //변수search의 input 태그의 값을 value를 넣어준다
 
